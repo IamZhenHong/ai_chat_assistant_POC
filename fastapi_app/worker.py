69e8e3930771b3508e58c80bef7a94e9f74787d8
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2025-02-25 16:01:04 +0800

    Update Dockerfile

--- fastapi_app/worker.py
+++ fastapi_app/worker.py
@@ -14,10 +14,11 @@ load_dotenv()
 # Celery setup with Redis
 
 REDIS_URL = os.environ.get("REDIS_URL")
-
+broker_use_ssl = {"ssl_cert_reqs": "CERT_NONE"} if REDIS_URL.startswith("rediss://") else None
 celery_app = Celery(
     "worker",
     broker=REDIS_URL,  # Redis as the message broker
+    broker_use_ssl = brpker_use_ssl
 )
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
